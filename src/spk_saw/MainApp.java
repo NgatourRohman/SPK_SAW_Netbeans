e89cd246521b0a237f7ce4daf30cbac40befd444
Ngatour Rohman <ngatourrohman25@gmail.com>
2025-06-05 12:37:27 +0700

    Enhancement: Integrate FlatLaf Modern UI Look and Feel

--- src/spk_saw/MainApp.java
+++ src/spk_saw/MainApp.java
@@ -8,12 +8,21 @@ package spk_saw;
  *
  * @author ngato
  */
-import javax.swing.SwingUtilities;
+import com.formdev.flatlaf.FlatDarkLaf;
+//import com.formdev.flatlaf.FlatLightLaf;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
 import spk_saw.views.DashboardForm;
 
 public class MainApp {
     public static void main(String[] args) {
-        SwingUtilities.invokeLater(() -> {
+        try {
+            UIManager.setLookAndFeel(new FlatDarkLaf()); // aktifkan FlatLaf nighmode (new FlatLightLaf()
+        } catch (UnsupportedLookAndFeelException e) {
+            e.printStackTrace();
+        }
+
+        javax.swing.SwingUtilities.invokeLater(() -> {
             new DashboardForm().setVisible(true);
         });
     }
